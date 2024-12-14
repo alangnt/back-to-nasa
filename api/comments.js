529db86a12e2c050adb074c67f7f2c307761f082
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-12-14 11:50:51 +0100

    fix: node not working

--- api/comments.js
+++ api/comments.js
@@ -9,15 +9,12 @@ module.exports = (req, res) => {
         // Format the comment
         const commentData = `Name: ${name}\nComment: ${comment}\n--------------------------------------\n`;
 
-        // Write to comments.txt (Note: Vercel does not persist the filesystem, so consider using a cloud database or storage solution)
-        fs.appendFile('comments.txt', commentData, (err) => {
+        // Write to comments-list.txt
+        fs.appendFile('comments-list.txt', commentData, (err) => {
             if (err) {
                 console.error('Error writing to file', err);
                 res.status(500).send('Server error');
-                return;
             }
-            console.log('Comment added!');
-            res.redirect('/');
         });
     } else {
         // If not a POST request, return an error

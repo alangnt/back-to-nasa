9721273dc55c9268b104b159a1c33c34793dba7e
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-12-14 12:26:57 +0100

    fix: added MongoDB connection

--- api/comments.js
+++ api/comments.js
@@ -1,23 +1,39 @@
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Comment = require('../models/Comment');
+
+// MongoDB URI
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 module.exports = (req, res) => {
     if (req.method === 'POST') {
-        // Parse the body of the POST request
         const { name, comment } = req.body;
 
-        // Format the comment
-        const commentData = `Name: ${name}\nComment: ${comment}\n--------------------------------------\n`;
-
-        // Write to comments-list.txt
-        fs.appendFile('comments-list.txt', commentData, (err) => {
-            if (err) {
-                console.error('Error writing to file', err);
-                res.status(500).send('Server error');
-            }
+        // Create a new comment document
+        const newComment = new Comment({
+            name: name,
+            comment: comment,
         });
+
+        // Save the comment to MongoDB
+        newComment.save()
+            .then(() => {
+                console.log('Comment saved to database');
+                res.redirect('/');  // Redirect to the homepage after saving
+            })
+            .catch((err) => {
+                console.error('Error saving comment', err);
+                res.status(500).send('Server error');
+            });
     } else {
-        // If not a POST request, return an error
         res.status(405).send('Method Not Allowed');
     }
 };
